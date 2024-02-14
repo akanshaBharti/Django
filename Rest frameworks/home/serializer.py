aff5259f9b9c6b14858eaa949f3b197f3a561043
Akansha <akanshabharti1213@gmail.com>
2024-02-14 14:17:06 +0530

    started serializer

--- Rest frameworks/home/serializer.py
+++ Rest frameworks/home/serializer.py
@@ -3,5 +3,5 @@ from .models import Person
 
 class PeopleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Person
+        model = Person 
         fields = '__all__'   # to add all fields use __all__, to exclude some fields use exclude = ['fieldname', 'fieldname']
--- Rest frameworks/home/views.py
+++ Rest frameworks/home/views.py
@@ -39,13 +39,41 @@ def index(request):
     #     print('you hit a PUT method')
     #     return Response(courses)
     
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def person(request):
-    if request.GET:
+    if request.method == "GET":
         objs = Person.objects.all()
         serializer = PeopleSerializer(objs, many = True)
         return Response(serializer.data)
-    else:
+    elif request.method == "POST":
         data = request.data
         serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    elif request.method == "PUT":     # passing all data whether updating or not 
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    elif request.method == "PATCH":   # partial updating, the field which is to be updated is passed
+        data = request.data
+        obj = Person.objects.get(id = data['id'])
+        serializer = PeopleSerializer(obj, data = data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    else:
+        data = request.data
+        obj = Person.objects.get(id = data['id'])
+        obj.delete()
+        return Response({'message': 'person deleted'})
+
     
