42dadf54dd43c7648ad47bcd31a8d68f1383a091
Akansha <akanshabharti1213@gmail.com>
2024-03-08 17:40:00 +0530

    api views

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Person, Color
 
 
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+
 class ColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Color
@@ -9,12 +14,18 @@ class ColorSerializer(serializers.ModelSerializer):
 
 class PeopleSerializer(serializers.ModelSerializer):
     color = ColorSerializer()
+    color_info = serializers.SerializerMethodField()
     class Meta:
         model = Person
         # fields = ['age']
         fields = '__all__'
         # exclude = ['name']  #to exclude a particular field
         # depth = 1
+        
+    def get_color_info(self, obj):
+        color_obj = Color.objects.get(id=obj.color.id)
+        return {'color_name': color_obj.color_name, 'hex_code' : '#000'}
+        # return "India"
         
     def validate(self, data):
         special_characters = '[^\\\\/`~!@#$%^&*:*?\"<>|.]*'
@@ -23,7 +34,7 @@ class PeopleSerializer(serializers.ModelSerializer):
         
         
         
-        if data['age'] < 18:
-            raise serializers.ValidationError('age should be greater than 18')
+        # if data.get('age') and data['age'] < 18:
+        #     raise serializers.ValidationError('age should be greater than 18')
         
         return data
--- core/home/views.py
+++ core/home/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from home.models import Person
 from home.serializers import PeopleSerializer
 
+from rest_framework.views import APIView
+from rest_framework import viewsets
 # Create your views here.
 
 @api_view(['GET', 'POST'])
@@ -27,11 +29,67 @@ def index(request):
        
     return Response(courses)
 
+@api_view(['POST'])
+def login(request):
+    data = request.data
+    serializer = LoginSerializer(data=data)
+    
+    if serializer.is_valid():
+        data = serializer.data
+        print(data)
+        return Response({'message' : 'success'})
+        
+    return Response(serializer.errors)
+
+class PersonAPI(APIView):
+    
+    def get(self, request):
+        objs = Person.objects.filter(color__isnull = False)
+        serializer = PeopleSerializer(objs, many=True)
+        return Response(serializer.data)
+        # return Response({'message': 'This is a get request'})
+    
+    def post(self, request):
+        data = request.data
+        serializer = PeopleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    
+    def put(self, request):
+        data = request.data
+        serializer = PeopleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    
+    def patch(self, request):
+        data = request.data
+        obj = Person.objects.get(id = data['id'])
+        serializer = PeopleSerializer(obj, data=data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    
+    def delete(self, request):
+        data = request.data
+        obj = Person.objects.get(id = data['id'])
+        obj.delete()
+        return Response({'message' : 'data deleted'})
+
+
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def person(request):
     if request.method == 'GET':
         objs = Person.objects.filter(color__isnull = False)
         serializer = PeopleSerializer(objs, many=True)
+        
         return Response(serializer.data)
     elif request.method == 'POST':
         data = request.data
@@ -65,3 +123,8 @@ def person(request):
         obj = Person.objects.get(id = data['id'])
         obj.delete()
         return Response({'message' : 'data deleted'})
+
+
+class PeopleViewSet(viewsets.ModelViewSet):
+    serializer_class = PeopleSerializer
+    queryset = Person.objects.all()
